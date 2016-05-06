035ef1f5188ef0d575de03da25c11b6a2716d3ac
sash <sash@bitbattalion.com>
2016-05-06 13:29:32 -0400

    adds in the third problem

---
+++ problem3/ThingDoer.hx
@@ -0,0 +1,121 @@
+/*
+  ThingDoer takes a thing, and a promise, and does the thing when the promise succeeds.
+  What are the potential erros in this code?
+  Refactor to avoid errors
+*/
+
+class ThingDoer
+{
+    public static function main() : Void {
+        var prom = new Promise<String>();
+        var doer = new ThingDoer();
+        var thing = new Thing(1);
+
+        var t = new haxe.Timer(1000);
+
+        doer.doThing(thing, prom);
+        haxe.Timer.delay( function() { Math.random() < 0.5 ? prom.setSuccess("succeeded") : prom.setFailure(); }, 1000);
+    }
+
+
+    public var didThing:Bool;
+    private var _thing:IThing;
+
+    public  function new() : Void {}
+
+    public function doThing<T>(thing:IThing, promise:Promise<T> = null):Void
+    {
+        _thing = thing;
+        if(promise == null) onSetComplete();
+        promise.onSuccess(onSetComplete);
+        promise.onFailure(onFailure);
+    }
+
+
+    public function isDoingThing():Bool
+    {
+        return _thing != null && !didThing;
+    }
+
+
+    private function onSetComplete():Void
+    {
+        _thing.doThing();
+        _thing = null;
+        didThing = true;
+    }
+
+
+    private function onFailure():Void
+    {
+        trace("failed");
+        _thing = null;
+    }
+}
+
+
+class Thing implements IThing
+{
+    private var _value:Int;
+
+    public function new(value:Int) : Void
+    {
+        _value = value;
+    }
+
+    public function doThing() : Void
+    {
+        trace("DID THING!" + _value);
+    }
+}
+
+
+interface IThing
+{
+    public function doThing():Void;
+}
+
+
+class Promise<T>
+{
+    public  var didSucceed:Bool;
+
+    private var _value:T;
+    private var _success:Void->Void;
+    private var _failure:Void->Void;
+
+    public function new():Void
+    {
+        didSucceed = false;
+    }
+
+    public function setSuccess(value:T) : Void
+    {
+        _value = value;
+        didSucceed = true;
+        if(_success != null) _success();
+    }
+
+    public function setFailure() : Void
+    {
+        didSucceed = false;
+        if(_failure != null) _failure();
+    }
+
+    public function onFailure(callback:Void->Void) : Void
+    {
+        _failure = callback;
+    }
+
+    public function onSuccess(callback:Void->Void) : Void
+    {
+        _success = callback;
+        if(didSucceed) _success();
+    }
+
+    public function getValue() :T
+    {
+        if(!didSucceed) throw "can't get value of failed future";
+        return _value;
+    }
+}
